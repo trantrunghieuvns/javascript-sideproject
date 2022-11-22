6ae7fd1cbe8cc52cb304d84ee913ee7299b62d77
trantrunghieuvns <trantrunghieuvns@gmail.com>
2022-11-23 06:24:25 +0700

    commit all

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,119 +1,116 @@
-import axios from "axios";
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import requests from "../Request";
-import TrailerMovie from "./Trailers/TrailerMovie";
-import { truncateString } from "./actions/truncateString";
-import { AiOutlineClose } from "react-icons/ai";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import "../CSS/slider.css";
+import axios from 'axios';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import requests from '../Request';
+import TrailerMovie from './Trailers/TrailerMovie';
+import { truncateString } from './actions/truncateString';
+import { AiOutlineClose } from 'react-icons/ai';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import '../CSS/slider.css';
 
 const Main = () => {
-  //react-Slick setting
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 1100,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    centerPadding: "100px",
-    autoplaySpeed: 1800,
-    cssEase: "linear",
-    fade: true,
-    pauseOnHover: true,
-  
-  };
-  //end of react-Slick css
+	//react-Slick setting
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 1100,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		centerPadding: '100px',
+		autoplaySpeed: 1800,
+		cssEase: 'linear',
+		fade: true,
+		pauseOnHover: true,
+	};
+	//end of react-Slick css
 
-  const [movies, setMovies] = useState([]);
+	const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    axios.get(requests.requestTrending).then((response) => {
-      setMovies(response.data.results);
-    });
-  }, []);
+	useEffect(() => {
+		axios.get(requests.requestTrending).then((response) => {
+			setMovies(response.data.results);
+		});
+	}, []);
 
-  const [movie, setMovie] = useState([]);
+	const [movie, setMovie] = useState([]);
 
-  useEffect(() => {
-    const movie = movies[Math.floor(Math.random() * movies.length)];
-    setMovie(movie);
-  }, [movies]);
+	useEffect(() => {
+		const movie = movies[Math.floor(Math.random() * movies.length)];
+		setMovie(movie);
+	}, [movies]);
 
-  // const imgPath = movie?.backdrop_path;
+	// const imgPath = movie?.backdrop_path;
 
-  const [trailer, setTrailer] = useState(false);
-  const [movieTitle, setMovieTitle] = useState();
+	const [trailer, setTrailer] = useState(false);
+	const [movieTitle, setMovieTitle] = useState();
 
-  const MovieTitle = (movie) => {
-    setMovieTitle(movie.title ? movie.title : movie.name);
-    setTrailer(true);
-  };
+	const MovieTitle = (movie) => {
+		setMovieTitle(movie.title ? movie.title : movie.name);
+		setTrailer(true);
+	};
 
-  return (
-    <>
-      <div className="slider_main w-full h-[560px] text-white mb-6">
-        <Slider {...settings}>
-          {movies.map((movie) => (
-            <div className="desc-main w-full h-[560px]">
-              <div className="blackBg "></div>
-              <img
-                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-                alt={`watch ${movie.title} online`}
-              />
-              <div className="description absolute w-full top-[20%] p-4 md:p-8 md:-mt-7 sm:mt-5 su:w-[30%] md:w-[90%] md:max-w-[80%] lg:max-w-[85%]">
-                <h1 className="font-black py-5 ml-0 md:text-5xl su:text-3xl sm:text-4xl">
-                  {movie?.title ? movie.title : movie.name}
-                </h1>
-                <div>
-                  <button
-                    onClick={() => {
-                      MovieTitle(movie);
-                    }}
-                    className="border bg-gray-300 text-black border-gray-300 py-2 px-5 "
-                  >
-                    Play
-                  </button>
-                  <button className="border text-white border-gray-300 py-2 px-5 ml-4">
-                    Watch Later
-                  </button>
-                </div>
-                <p className="text-gray-400 text-sm mt-4">
-                  Release: {movie?.release_date}
-                </p>
+	return (
+		<>
+			<div className='slider_main z-0   w-full h-[560px] text-white mb-6'>
+				<Slider {...settings}>
+					{movies.map((movie) => (
+						<div className='desc-main w-full h-[560px]'>
+							<div className='blackBg '></div>
+							<img
+								src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+								alt={`watch ${movie.title} online`}
+							/>
+							<div className='description absolute w-full top-[20%] p-4 md:p-8 md:-mt-7 sm:mt-5 su:w-[30%] md:w-[90%] md:max-w-[80%] lg:max-w-[85%]'>
+								<h1 className='font-black py-5 ml-0 md:text-5xl su:text-3xl sm:text-4xl'>
+									{movie?.title ? movie.title : movie.name}
+								</h1>
+								<div>
+									<button
+										onClick={() => {
+											MovieTitle(movie);
+										}}
+										className='border bg-gray-300 text-black border-gray-300 py-2 px-5 '>
+										Play
+									</button>
+									<button className='border text-white border-gray-300 py-2 px-5 ml-4'>
+										Watch Later
+									</button>
+								</div>
+								<p className='text-gray-400 text-sm mt-4'>
+									Release: {movie?.release_date}
+								</p>
 
-                <p className="text-gray-300 text-sm sm:mt-5 su:w-[60%] md:w-[90%] md:max-w-[80%] lg:max-w-[85%] ">
-                  Overview: {truncateString(movie?.overview, 150)}
-                </p>
-              </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
+								<p className='text-gray-300 text-sm sm:mt-5 su:w-[60%] md:w-[90%] md:max-w-[80%] lg:max-w-[85%] '>
+									Overview: {truncateString(movie?.overview, 150)}
+								</p>
+							</div>
+						</div>
+					))}
+				</Slider>
+			</div>
 
-      {trailer ? (
-        <div
-          onClick={() => setTrailer(false)}
-          className="z-50  w-screen fixed inset-0 bg-black/90 text-white/90 "
-        >
-          <div className="flex player items-center justify-center h-screen w-screen">
-            <TrailerMovie movieTitle={movieTitle} movie={movie} />
-            <AiOutlineClose
-              id={movie.title}
-              className="fixed mx-[8em] -my-[11em] md:mx-[23em] md:-my-[9em]  lg:mx-[33em] lg:-my-[18em] xl:mx-[33em] xl:-my-[18em] cursor-pointer"
-              onClick={() => setTrailer(false)}
-            />
-          </div>
-        </div>
-      ) : (
-        <></>
-      )}
-    </>
-  );
+			{trailer ? (
+				<div
+					onClick={() => setTrailer(false)}
+					className='z-50 w-screen overflow:hidden fixed inset-0 bg-black/90 text-white/90 '>
+					<div className='flex player fixed items-center justify-center h-screen w-screen'>
+						<TrailerMovie movieTitle={movieTitle} movie={movie} />
+						<AiOutlineClose
+							id={movie.title}
+							className='fixed mx-[8em] -my-[11em] md:mx-[23em] md:-my-[9em]  lg:mx-[33em] lg:-my-[18em] xl:mx-[33em] xl:-my-[18em] cursor-pointer'
+							onClick={() => setTrailer(false)}
+						/>
+					</div>
+				</div>
+			) : (
+				<></>
+			)}
+		</>
+	);
 };
 
 export default Main;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
-import { UserAuth } from "../context/AuthContext";
-import { AiOutlineSearch } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
-import { useState, useRef } from "react";
+import { Link } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { useState, useRef } from 'react';
 
 const Navbar = ({ movies, searchResults, setSearchResults }) => {
 	const navigate = useNavigate();
-	const inputRef = useRef("");
-	const [term, setTerm] = useState("");
+	const inputRef = useRef('');
+	const [term, setTerm] = useState('');
 	const { user, logOut } = UserAuth();
 
 	const handleLogOut = async () => {
@@ -20,10 +20,10 @@ const Navbar = ({ movies, searchResults, setSearchResults }) => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		navigate("/search");
+		navigate('/search');
 		if (!term) return;
 		setSearchResults(term);
-		inputRef.current.value = "";
+		inputRef.current.value = '';
 	};
 
 	const handleSearchChange = (e) => {
@@ -31,54 +31,53 @@ const Navbar = ({ movies, searchResults, setSearchResults }) => {
 	};
 
 	return (
-		<div className="flex justify-between p-4 z-[100] w-full absolute">
-			<Link to="/">
-				<h1 className="text-red-600 text-2xl font-bold cursor-pointer ml-[1rem]">
+		<div className='flex justify-between p-4 z-[50] w-full absolute'>
+			<Link to='/'>
+				<h1 className='text-red-600 text-2xl font-bold cursor-pointer ml-[1rem]'>
 					NETFLIX
 				</h1>
 			</Link>
 
 			{user?.email ? ( // 2 situations
 				<div>
-					<div className="my-10 absolute md:my-0 md:-mt-3 md:-m-[30%] lg:-mx-[20%] opacity-70 hover:opacity-80 transition-all transition-duration: 350ms">
-						<div className="search-bar">
+					<div className='my-10 absolute md:my-0 md:-mt-3 md:-m-[30%] lg:-mx-[20%] opacity-70 hover:opacity-80 transition-all transition-duration: 350ms'>
+						<div className='search-bar'>
 							<form onSubmit={handleSubmit}>
 								<input
 									ref={inputRef}
-									className="placeholder:px-3 bg-slate-700 py-1 mt-4 rounded cursor-pointer text-white w-[100%] -mr-1 md:-mr-8 "
-									type="text"
+									className='placeholder:px-3 bg-slate-700 py-1 mt-4 rounded cursor-pointer text-white w-[100%] -mr-1 md:-mr-8 '
+									type='text'
 									placeholder={`Search...`}
 									onChange={handleSearchChange}
 								/>
 
-								<div className="text-white">
-									<button type="submit" className="block">
-										<AiOutlineSearch className=" absolute -my-6 right-3 block" />
+								<div className='text-white'>
+									<button type='submit' className='block'>
+										<AiOutlineSearch className=' absolute -my-6 right-3 block' />
 									</button>
 								</div>
 							</form>
 						</div>
 					</div>
 
-					<Link to="/account">
-						<button className="text-white pr-5">Account</button>
+					<Link to='/account'>
+						<button className='text-white pr-5'>Account</button>
 					</Link>
-					<Link to="/">
+					<Link to='/'>
 						<button
-							className="bg-red-600  px-5 py-2 rounded cursor-pointer text-white"
-							onClick={handleLogOut}
-						>
+							className='bg-red-600  px-5 py-2 rounded cursor-pointer text-white'
+							onClick={handleLogOut}>
 							Log Out
 						</button>
 					</Link>
 				</div>
 			) : (
 				<div>
-					<Link to="/login">
-						<button className="text-white pr-5">Sign In</button>
+					<Link to='/login'>
+						<button className='text-white pr-5'>Sign In</button>
 					</Link>
-					<Link to="/signup">
-						<button className="bg-red-600  px-5 py-2 rounded cursor-pointer text-white">
+					<Link to='/signup'>
+						<button className='bg-red-600  px-5 py-2 rounded cursor-pointer text-white'>
 							Sign Up
 						</button>
 					</Link>
